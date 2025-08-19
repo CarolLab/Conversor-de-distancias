05e5048a5ccedc7cfdd6e27d0c29ae23d2c92679
CarolLab <bessascarolina@gmail.com>
2025-08-19 17:20:31 +0100

    Associação de uma varaiável de controlo ao label 'distancia_2'

--- Conversor_de_distancias.py
+++ Conversor_de_distancias.py
@@ -4,39 +4,39 @@ import tkinter as tk
 def obter_unidade_lstb2():#Obter as unidades selecionadas da listbox 2
     posicao = listbx_2.curselection() #Posição da unidade na listbox2
     if posicao: #se há algum item selecionado
-        unidade_label2["text"] = listbx_2.get(posicao) #Obtem a unidade selecionada e
+        unidade2_label2["text"] = listbx_2.get(posicao) #Obtem a unidade selecionada e
         # muda o texto da label para a unidade
 
 
 def obter_unidade_lstb1(): #Obter as unidades selecionadas da listbox 1
     posicao = listbx_1.curselection() #Recebe a posição da unidade na listbox1 em tupla
     if posicao: #se há algum item selecionado
-        unidade_label1["text"] = listbx_1.get(posicao)#Obtém a unidade selecionada e
+        unidade1_label1["text"] = listbx_1.get(posicao)#Obtém a unidade selecionada e
         #muda o texto da label para a unidade
 
 
 
 def mostrar_resultado(distancia):
-    global unidade_label1  # Unidade 1 escolhida
-    global unidade_label2  # Unidade 2 escolhida
+    global unidade1_label1  # Unidade 1 escolhida
+    global unidade2_label2  # Unidade 2 escolhida
 
     label_erro.config(text="")
 
     try:
         # Obter números e as unidades
         num_converter = float(distancia.get())  # Obter o número a converter
-        unidade1 = unidade_label1.cget("text")  # Obter a unidade selecionada a ser convertido
-        unidade2 = unidade_label2.cget("text")
+        unidade1 = unidade1_label1.cget("text")  # Obter a unidade selecionada a ser convertido
+        unidade2 = unidade2_label2.cget("text")
 
         if unidade1 and unidade2: #Se as duas unidades foram escolhidas
             if unidade1 == unidade2: #Verificar se são iguais
-                distancia_2["text"] = num_converter #O número é igual
+                var_distancia_2.set(str(num_converter))#O número é igual
             else:
                 #Converter o número
                 resultado = converter(unidade1,unidade2, num_converter)
 
                 #Mostrar o resultado
-                distancia_2["text"] = resultado
+                var_distancia_2.set(str(resultado))
         else:
             label_erro["text"] = "Escolha as unidades"
 
@@ -120,11 +120,13 @@ titulo.place(relx = 0.5, rely = 0.5, anchor = "center")
 
 
 #Distâncias - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#Variáveis de controlo
+var_distancia_2 = tk.StringVar()#Variável de controlo do label distancia_2
 #Entrys
 distancia_1 = tk.Entry(frame_2, relief = "solid", bd = 1,
                        font = "Arial 13") #Número para converter
-distancia_2 = tk.Label(frame_2, text = "", bd = 2, relief = "solid", bg = "White",
-                       font = "Calibri 13")#Número convertido
+distancia_2 = tk.Label(frame_2, bd = 2, relief = "solid", bg = "White",
+                       font = "Calibri 13", textvariable=var_distancia_2)#Número convertido
 
 distancia_1.place(relx = 0.05, rely = 0.25, relwidth = 0.30, relheight = 0.12)
 distancia_2.place(relx = 0.65, rely = 0.25, relwidth = 0.30, relheight = 0.12)
@@ -147,8 +149,8 @@ listbx_2 = tk.Listbox(frame_2,bd = 2, relief = "solid", font = ("Arial", 13), ju
                      selectmode = "browse", listvariable = variavel_listbx2)
 
 #Labels
-unidade_label1 = tk.Label(frame_2, text = "", font = "Tahoma") #Label da unidade 1 (lista_1)
-unidade_label2 = tk.Label(frame_2, text = "", font = "Tahoma") #Label da unidade 2 (lista_2)
+unidade1_label1 = tk.Label(frame_2, text = "", font = "Tahoma") #Label da unidade 1 (lista_1)
+unidade2_label2 = tk.Label(frame_2, text = "", font = "Tahoma") #Label da unidade 2 (lista_2)
 
 
 
@@ -160,8 +162,8 @@ listbx_2.bind("<<ListboxSelect>>", lambda event: obter_unidade_lstb2())
 listbx_1.place(relx = 0.19, rely = 0.47, relwidth = 0.1, relheight = 0.22,anchor = "n")
 listbx_2.place(relx = 0.80, rely = 0.47,relwidth = 0.1,relheight = 0.22, anchor = "n")
 
-unidade_label1.place(relx = 0.19, rely = 0.15, anchor = "center")
-unidade_label2.place(relx = 0.80, rely = 0.15, anchor = "center")
+unidade1_label1.place(relx = 0.19, rely = 0.15, anchor = "center")
+unidade2_label2.place(relx = 0.80, rely = 0.15, anchor = "center")
 
 #Botão - - - - - - - - - - - - - - - - - - - - - - - - -  - - -
 botao = tk.Button(frame_2, width=15, height = 1, cursor = "hand2",
